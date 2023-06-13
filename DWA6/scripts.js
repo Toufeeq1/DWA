325874b71ae9446e37b0a9b6c21afab4d60a0d08
Mogamat Toufeeq farat <toufeeqfarat@gmail.com>
2023-06-13 08:18:34 +0200

    completed DWA6

---
+++ DWA6/scripts.js
@@ -0,0 +1,36 @@
+// @ts-check
+
+import { state } from "./state.js";
+console.log("It works");
+
+const list = document.querySelector('[data-list]')
+const isHtmlElemnt = list instanceof HTMLElement
+
+if (!isHtmlElemnt) {
+    throw new Error ('"data-list" attribute not found in HTML');
+}
+
+const preview = document.createElement('li')
+preview.className = "task";
+
+preview.innerHTML = /* html */ `
+<label class="task__check">
+<input class="task__input" type="checkbox" />
+</label>
+<button class="task__title">
+Wash th Dog asda sdasd asd as ddas Dog asda sdasd asd as ddase Dog
+asda sdasd asd as ddas
+</button>
+<label class="task__check">
+<svg
+  class="task__icon"
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 96 960 960"
+>
+  <path
+    d="M253 961q-40.212 0-67.606-27.1Q158 906.8 158 867V314h-58v-94h231v-48h297v48h232v94h-58v553q0 39.05-27.769 66.525Q746.463 961 707 961H253Zm454-647H253v553h454V314ZM354 789h77V390h-77v399Zm175 0h78V390h-78v399ZM253 314v553-553Z"
+  ></path>
+</svg>
+</label>
+`;
+list.appendChild(preview) ;
